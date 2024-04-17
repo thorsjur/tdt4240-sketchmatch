6f205b3f9deab605389a86c76581d04503134fae
Tsvetelin Petrov <42267885+tsvetelinpetrov@users.noreply.github.com>
2024-04-17 14:01:32 +0200

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         }
 
         debug {
-            buildConfigField("String", "SOCKET_IO_ADDRESS", "\"https://creativecode.tu-varna.bg:40401\"")
+            buildConfigField("String", "SOCKET_IO_ADDRESS", "\"http://creativecode.tu-varna.bg:40401\"")
         }
     }
     compileOptions {
